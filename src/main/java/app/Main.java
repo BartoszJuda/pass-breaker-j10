cbbd9564336f66dbd1ac91fc57b180ada7b3113d
Bartosz <juda.bartosz@gmail.com>
2019-08-27 20:15:49 +0200

    skonfigurowano passBreaker

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+
+        PassBreaker passBreaker=new PassBreaker();
+        passBreaker.passBreaker("Kolor");
     }
 }
 
